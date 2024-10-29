6af80a0d8ea099a52a1397aa4c62ff95405ac051
YANCX-dev <ybabuskin@gmail.com>
2024-10-29 19:14:29 +0300

    first commit

---
+++ pages/header.php
@@ -0,0 +1,14 @@
+<header class="header">
+    <div class="header__banner">
+        <p class="header__banner-text">Use this banner to inform your users of something important.</p>
+    </div>
+    <nav class="header__nav">
+        <ul class="header__menu">
+            <li class="header__menu-item"><a href="#" class="header__menu-link">Overview</a></li>
+            <li class="header__menu-item"><a href="#" class="header__menu-link">Pages</a></li>
+            <li class="header__menu-item"><a href="#" class="header__menu-link">Services</a></li>
+            <li class="header__menu-item"><a href="#" class="header__menu-link">Contact</a></li>
+        </ul>
+        <button class="header__button">Purchase Now</button>
+    </nav>
+</header>
